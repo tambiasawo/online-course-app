a9a258496758d60a621c18c78d6b6ccc73989dd0
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-20 11:12:17 -0700

    feat: project setup

---
+++ app/(router)/layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import SideNav from "./_components/SideNav";
+import Header from "./_components/Header";
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      <div className="hidden sm:w-64 sm:block fixed">
+        <SideNav />
+      </div>
+      <div className="sm:ml-64">
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default layout;
